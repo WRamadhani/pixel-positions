f4afba78c472e0951e1e5711eaed99de5d6fa8ce
WRamadhani <wahyuramadhani55@gmail.com>
2025-01-14 04:19:34 +0800

    add migration, factory, and seeder (also display data from database)

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,14 +1,15 @@
+@props(['job'])
 <x-panel class="flex flex-col text-center">
-    <div class="self-start text-sm">Laracast</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
     <div class="py-8">
-        <h3 class="text-xl font-bold transition-colors duration-200 group-hover:text-blue-600">Video Producer</h3>
-        <p class="mt-4 text-sm">Full Time - From $60,000</p>
+        <h3 class="text-xl font-bold transition-colors duration-200 group-hover:text-blue-600">{{ $job->title }}</h3>
+        <p class="mt-4 text-sm">{{ $job->schedule }} - {{ $job->salary }}</p>
     </div>
     <div class="flex items-center justify-between mt-auto">
         <div>
-            <x-tag size="small">Backend</x-tag>
-            <x-tag size="small">Frontend</x-tag>
-            <x-tag size="small">Manager</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag size="small" :$tag />
+            @endforeach
         </div>
 
         <x-employer-logo :width="42" />
--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,4 @@
-@props(['size' => 'base'])
+@props(['tag', 'size' => 'base'])
 @php
     $classes = 'font-bold transition-colors duration-300 bg-white/10 hover:bg-white/25 rounded-xl';
     if ($size == 'base') {
@@ -9,4 +9,4 @@
         $classes .= ' px-3 py-1 text-2xs';
     }
 @endphp
-<a href="#" class="{{ $classes }}">{{ $slot }}</a>
+<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ $tag->name }}</a>
